194d05cb43a4d4bc99e0bd46376df8f87b771f2a
Enesi Wise <enesiwise@gmail.com>
2023-08-26 13:04:26 +0100

    .

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,55 +1,72 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
 import { FooterOverlay, Newsletter } from "../../components";
 import { images } from "../../constants";
 import "./Footer.css";
 
-const Footer = () => (
-  <div className="app__footer section__padding" id="login">
-    <FooterOverlay />
-    <Newsletter />
+const Footer = () => {
+  useEffect(() => {
+    const toTop = document.getElementById("topBtn");
 
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
-      </div>
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 900) {
+        toTop.classList.add("active");
+      } else {
+        toTop.classList.remove("active");
+      }
+    });
+  }, []);
+  return (
+    <div className="app__footer section__padding" id="login">
+      <FooterOverlay />
+      <Newsletter />
 
-      <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">
-          &quot;The best way to find yourself is to lose yourself in the service
-          of others.&quot;
-        </p>
-        <img
-          src={images.spoon}
-          alt="spoon"
-          className="spoon__img"
-          style={{ marginTop: 15 }}
-        />
-        <div className="app__footer-links_icons">
-          <FiFacebook />
-          <FiTwitter />
-          <FiInstagram />
+      <div className="app__footer-links">
+        <div className="app__footer-links_contact">
+          <h1 className="app__footer-headtext">Contact Us</h1>
+          <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+          <p className="p__opensans">+1 212-344-1230</p>
+          <p className="p__opensans">+1 212-555-1230</p>
+        </div>
+
+        <div className="app__footer-links_logo">
+          <img src={images.gericht} alt="footer_logo" />
+          <p className="p__opensans">
+            &quot;The best way to find yourself is to lose yourself in the
+            service of others.&quot;
+          </p>
+          <img
+            src={images.spoon}
+            alt="spoon"
+            className="spoon__img"
+            style={{ marginTop: 15 }}
+          />
+          <div className="app__footer-links_icons">
+            <FiFacebook />
+            <FiTwitter />
+            <FiInstagram />
+          </div>
+        </div>
+
+        <div className="app__footer-links_work">
+          <h1 className="app__footer-headtext">Working Hours</h1>
+          <p className="p__opensans">Monday-Friday:</p>
+          <p className="p__opensans">08:00 am - 12:00 am</p>
+          <p className="p__opensans">Saturday-Sunday:</p>
+          <p className="p__opensans">07:00 am - 11:00 pm</p>
         </div>
       </div>
 
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      <div className="footer__copyright">
+        <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
       </div>
-    </div>
 
-    <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <a href="#top" className="top" id="topBtn">
+        <div className="top-dash"></div> TOP
+      </a>
     </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
